dd79d305d59f1b26b42b773501f6ade47ff0544d
Chupendo <andresruizpenuela@gmail.com>
2021-09-20 23:15:59 +0200

    created porltet basic
    Se crea un portlet básico que muestra el nombre del proyecto

---
+++ ws_maven_740/modules/HelloWorld/src/main/java/com/portlet/hello/world/constants/HelloWorldClassPortletKeys.java
@@ -0,0 +1,11 @@
+package com.portlet.hello.world.constants;
+
+/**
+ * @author andre
+ */
+public class HelloWorldClassPortletKeys {
+
+	public static final String HELLOWORLDCLASS =
+		"com_portlet_hello_world_HelloWorldClassPortlet";
+
+}
---
+++ ws_maven_740/modules/HelloWorld/src/main/java/com/portlet/hello/world/portlet/HelloWorldClassPortlet.java
@@ -0,0 +1,30 @@
+package com.portlet.hello.world.portlet;
+
+import com.portlet.hello.world.constants.HelloWorldClassPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author andre
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=HelloWorldClass",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + HelloWorldClassPortletKeys.HELLOWORLDCLASS,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class HelloWorldClassPortlet extends MVCPortlet {
+}

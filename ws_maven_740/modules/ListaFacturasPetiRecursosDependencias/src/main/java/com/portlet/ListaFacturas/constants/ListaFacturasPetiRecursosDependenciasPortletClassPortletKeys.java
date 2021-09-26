735dd1b5d30a19177fb66a624ae087183086ab0a
Chupendo <andresruizpenuela@gmail.com>
2021-09-26 18:03:03 +0200

    start portlet empaty

---
+++ ws_maven_740/modules/ListaFacturasPetiRecursosDependencias/src/main/java/com/portlet/ListaFacturas/constants/ListaFacturasPetiRecursosDependenciasPortletClassPortletKeys.java
@@ -0,0 +1,11 @@
+package com.portlet.ListaFacturas.constants;
+
+/**
+ * @author andre
+ */
+public class ListaFacturasPetiRecursosDependenciasPortletClassPortletKeys {
+
+	public static final String LISTAFACTURASPETIRECURSOSDEPENDENCIASPORTLETCLASS =
+		"com_portlet_ListaFacturas_ListaFacturasPetiRecursosDependenciasPortletClassPortlet";
+
+}

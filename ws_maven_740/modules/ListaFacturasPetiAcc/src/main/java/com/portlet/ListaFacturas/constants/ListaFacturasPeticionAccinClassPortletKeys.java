811320467396aebdc92e29414ad4a5ed352d4bbd
Chupendo <andresruizpenuela@gmail.com>
2021-09-26 10:08:25 +0200

    start portlet empaty

---
+++ ws_maven_740/modules/ListaFacturasPetiAcc/src/main/java/com/portlet/ListaFacturas/constants/ListaFacturasPeticionAccinClassPortletKeys.java
@@ -0,0 +1,11 @@
+package com.portlet.ListaFacturas.constants;
+
+/**
+ * @author andre
+ */
+public class ListaFacturasPeticionAccinClassPortletKeys {
+
+	public static final String LISTAFACTURASPETICIONACCINCLASS =
+		"com_portlet_ListaFacturas_ListaFacturasPeticionAccinClassPortlet";
+
+}
